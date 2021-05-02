136889e915d4f80ad535385a532f59254d02cc49
wojci <wojcikowsk@gmail.com>
2021-05-02 18:20:55 +0200

    Aggiungere i file di progetto.

---
+++ Main.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using Kingmaker;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Area;
+using Kingmaker.EntitySystem.Persistence;
+using UnityModManagerNet;
+
+namespace GameOver
+{
+	public class Main
+	{
+		public static bool Enabled;
+		static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			try
+			{
+				logger = modEntry.Logger;
+				modEntry.OnToggle = OnToggle;
+				var harmony = new Harmony(modEntry.Info.Id);
+
+				harmony.PatchAll(Assembly.GetExecutingAssembly());
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Main.Error(ex);
+			}
+			return true;
+		}
+
+		static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+		{
+			Enabled = value;
+			return true;
+		}
+
+		public static void Error(Exception ex)
+		{
+			if (Main.logger != null)
+			{
+				Main.logger.Error(ex.ToString());
+			}
+		}
+		internal static Exception Error(string message)
+		{
+			UnityModManager.ModEntry.ModLogger modLogger = Main.logger;
+			if (modLogger != null)
+			{
+				modLogger.Log(message);
+			}
+			return new InvalidOperationException(message);
+		}
+
+		internal static UnityModManager.ModEntry.ModLogger logger;
+		internal static bool enabled;
+		[HarmonyLib.HarmonyPatch(typeof(Game), "LoadNewGame", new[] { typeof(BlueprintAreaPreset), typeof(SaveInfo) })]
+		class LevelUpController_SetupNewCharacter_Patch
+		{
+			static void Postfix(ref Game __instance)
+			{
+				__instance.State.PlayerState.MainCharacter.Value.Descriptor.IsEssentialForGame = false;
+
+			}
+		}
+
+
+
+	}
+
+
+
+}
---
+++ RemoveSlumberHD.cs
@@ -0,0 +1,52 @@
+﻿using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic.Abilities.Blueprints;
+using CallOfTheWild;
+using CallOfTheWild.NewMechanics;
+using System;
+
+namespace GameOver
+{
+	public class RemoveSlumberHD
+	{
+		private static LibraryScriptableObject library
+		{
+			get
+			{
+				return Main.Library;
+			}
+		}
+
+		// Token: 0x06000025 RID: 37 RVA: 0x00002918 File Offset: 0x00000B18
+		public static void modSlumber()
+		{
+			try
+			{
+				foreach (string assetId in RemoveSlumberHD.guids)
+				{
+					BlueprintAbility blueprintAbility = RemoveSlumberHD.library.Get<BlueprintAbility>(assetId);
+					if (blueprintAbility != null)
+					{
+						blueprintAbility.RemoveComponents<AbilityTargetCasterHDDifference>();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return;
+			}
+			
+		}
+
+
+		public static string[] guids = new string[]
+		{
+			"31f0fa4235ad435e95ebc89d8549c2ce",
+			"b03f4347c1974e38acff99a2af092461",
+			"15d3a2eef8ac43dd886d2bae83be35eb",
+			"c04cde18e91e4f84898de92a372bc1e0",
+			"6535cf6ab2c143079468edb7e1cd2b86",
+			"e845d92965544e2ba9ca7ab5b1b246ca",
+			"656b4f5990f14f29b0e2c262a39d274f"
+		};
+	}
+}
